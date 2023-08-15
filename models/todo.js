178ad7810e14d96e20281cb221d070eec6cc2e1a
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-08-16 00:06:39 +0530

    backend-update

---
+++ models/todo.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+const todoSchema = new Schema(
+  {
+    title: String,
+    description: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Todo = mongoose.model.Todo || mongoose.model("Todo", todoSchema);
+
+export default Todo;
+
+// 24:47
